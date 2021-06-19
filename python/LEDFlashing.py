a22cea83dee7d9237f48b06e8bbb6c1ecde6c3de
TheSillySmoker <thesillysmoker@gmail.com>
2021-06-19 20:26:47 +1000

    test

--- python/LEDFlashing.py
+++ python/LEDFlashing.py
@@ -5,11 +5,11 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(ledPin, GPIO.OUT)
 
 x = 0
-while x < 20:
+while x < 50:
     GPIO.output(ledPin, GPIO.HIGH)
-    time.sleep(0.5)
+    time.sleep(0.1)
     GPIO.output(ledPin, GPIO.LOW)
-    time.sleep(0.5)
+    time.sleep(0.3)
     x += 1
         
 
