82cd25ad5b56239401eff42f436c3e2d3d820248
TheSillySmoker <thesillysmoker@gmail.com>
2021-06-19 21:52:05 +1000

    '

--- python/trial.py
+++ python/trial.py
@@ -5,24 +5,5 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(ledPin, GPIO.OUT)
 GPIO.output(ledPin, GPIO.HIGH)
 
-
-var = str(input("On or Off? Please enter: "))
-print(var)
-time.sleep(1)
-print(var)
-time.sleep(1)
-
-var1 = str(input("On or Off? Please enter: "))
-"""
-if var == "On":
-
-    GPIO.output(ledPin, GPIO.HIGH)
-    time.sleep(1)
-
-elif var == "Off":
-    GPIO.output(ledPin, GPIO.LOW)
-else:
-    print("Please enter either 'On' or 'Off'.")
-
-        
-"""
+username = input("Enter username:")
+print("Username is: " + username)
