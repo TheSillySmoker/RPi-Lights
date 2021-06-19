c46c935aacfd1c2dd596460553800acbd6059f45
TheSillySmoker <thesillysmoker@gmail.com>
2021-06-19 20:06:56 +1000

    bat files

--- Raspberry Pi light/bin/Debug/LEDOff.bat
+++ Raspberry Pi light/bin/Debug/LEDOff.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\PuTTY\putty.exe" -ssh pi@192.168.1.19 -pw pie -m "C:\Users\Mark Schroeder\source\repos\Raspberry Pi light\bin\Debug\Config\commandsOff.txt" -t
+"C:\Program Files\PuTTY\putty.exe" -ssh pi@192.168.1.19 -pw pie -m "C:\RPi-Lights\Raspberry Pi light\bin\Debug\Config\commandsOff.txt" -t
