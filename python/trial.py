a17cdc2b18771ac318d378513f2720bbc2a8d0a4
TheSillySmoker <thesillysmoker@gmail.com>
2021-06-19 21:31:09 +1000

    triallll

--- python/trial.py
+++ python/trial.py
@@ -3,19 +3,20 @@ import time
 ledPin = 21
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(ledPin, GPIO.OUT)
-x = 1
-while x == 1:
+
+
+    
+var = input("On or Off? Please enter: ")
+print(var)
+if var == "On":
+
+    GPIO.output(ledPin, GPIO.HIGH)
+    time.sleep(1)
+
+elif var == "Off":
+    GPIO.output(ledPin, GPIO.LOW)
+else:
+    print("Please enter either 'On' or 'Off'.")
+
         
-    var = input("On or Off? Please enter: ")
-    if var == "On":
-
-        GPIO.output(ledPin, GPIO.HIGH)
-        time.sleep(1)
-
-    elif var == "Off":
-        GPIO.output(ledPin, GPIO.LOW)
-    else:
-        print("Please enter either 'On' or 'Off'.")
-
-            
 
