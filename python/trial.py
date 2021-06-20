fabcaaf403fa5a4d6ac99c14be90370a178ca472
TheSillySmoker <thesillysmoker@gmail.com>
2021-06-20 13:43:35 +1000

    close

--- python/trial.py
+++ python/trial.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
 import time
+import sys
 ledPin = 21
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(ledPin, GPIO.OUT)
@@ -9,10 +10,12 @@ while x == 1:
     var = raw_input("On or Off? Please enter: ")
     if var == "On":
         GPIO.output(ledPin, GPIO.HIGH)
-        print("The LED is now " + var + "!")
+        print("The LED is now " + var + "! Type 'Exit' to close program.")
     elif var == "Off":
         GPIO.output(ledPin, GPIO.LOW)
-        print("The LED is now " + var + "!")
+        print("The LED is now " + var + "! Type 'Exit' to close program.")
+    elif var == "Exit":
+        sys.exit()
     else:
         print("Please enter either 'On' or 'Off'.")
 
